15c2929125ae619eca68f34c0382fa7d9a71f2dd
revsic <revsic99@gmail.com>
2024-07-04 13:49:00 +0000

    run.sh: Add default running script

---
+++ run.sh
@@ -0,0 +1,19 @@
+nproc=8
+PROJECT=libpcap
+
+# pushd ./data/${PROJECT}
+# ./build.sh
+# popd
+
+#  the following command is sufficient to perform fuzzing
+cargo run --bin fuzzer -- ${PROJECT} -c $(nproc) -r
+# you can run this command to fuse the programs into a fuzz driver that can be fuzzed:
+cargo run --bin harness -- ${PROJECT} fuse-fuzzer
+# And, you can execute the fuzzers you fused:
+cargo run --bin harness -- ${PROJECT} fuzzer-run
+
+# # After 24 hours execution(s), you should deduplicate the reported crashes by PromptFuzz:
+# cargo run --bin harness -- ${PROJECT} sanitize-crash
+# # Then, you can collect and verbose the code coverage of your fuzzers by:
+# cargo run --bin harness -- ${PROJECT} coverage collect
+# cargo run --bin harness -- ${PROJECT} coverage report
